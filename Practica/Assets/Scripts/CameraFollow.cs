525f9e31eea77e6541c4a7e6fe12c6406c905b3a
Kevin <alu0100789461@ull.edu.es>
2016-09-30 11:48:48 +0100

    Add project
    Initial files

---
+++ Practica/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Pelota");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.LookAt (player.transform);
+		Vector3 pos = transform.position;
+		pos.y = transform.position.y;
+		pos.z = player.transform.position.z - 30;
+		transform.position = pos;
+	}
+}
---
+++ Practica/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour {
+	private Rigidbody body;
+	public float speed = 20;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 move = new Vector3(horizontal, 0f, vertical);
+		body.AddForce(move * speed);
+	}
+}
